43150618b06e43cb0b129f815fe1b2aa019fd683
Marlene <41800372+mardb@users.noreply.github.com>
2018-10-13 21:57:37 -0700

    initial commit
    basic function. Added labels with questions and answers. Added a tap function to reveal the answer.

---
+++ flashcards/flashcards/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  flashcards
+//
+//  Created by Marlene  on 10/13/18.
+//  Copyright © 2018 Marlene Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+         frontLabel.isHidden = true
+    }
+}
+
