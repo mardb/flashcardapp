acd7d75b808dd7f5c1fd6740dc0b490723af6ff1
Marlene Guzman <marleneguzmanla@gmail.com>
2018-10-27 22:47:33 -0700

    add multiple choices

--- flashcards/CreationViewController.swift
+++ flashcards/CreationViewController.swift
@@ -14,6 +14,8 @@ class CreationViewController: UIViewController {
     
     @IBOutlet weak var questionTextField: UITextField!
     @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var extraAnswer1: UITextField!
+    @IBOutlet weak var extraAnswer2: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +30,10 @@ class CreationViewController: UIViewController {
     @IBAction func didTapOnDone(_ sender: Any) {
         let questionText = questionTextField.text
         let answerText = answerTextField.text
+        let extraAns1 = extraAnswer1.text
+        let extraAns2 = extraAnswer2.text
         
-        flashCardController.updateFlashcard(question: questionText!, answer: answerText!)
+        flashCardController.updateFlashcard(question: questionText!, answer: answerText!, extraAnswer1: extraAns1!, extraAnswer2: extraAns2!)
         dismiss(animated: true)
     }
     /*
